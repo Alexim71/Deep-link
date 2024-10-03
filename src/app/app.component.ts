fc55d5315007687d66f4f97c88fe20eee7b2950b
ALEXIMA Sanlove Eden <sanloveedena@gmail.com>
2024-10-03 15:23:12 -0400

    Deep-link ionic and angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { App, URLOpenListenerEvent } from '@capacitor/app';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+    constructor(private router: Router, private zone: NgZone) {
+        this.initializeApp();
+    }
+
+    initializeApp() {
+        App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
+            this.zone.run(() => {
+                const slug = event.url.split(".app").pop();
+                if (slug) {
+                    this.router.navigateByUrl(slug);
+                }
+            });
+        });
+    }
 }
